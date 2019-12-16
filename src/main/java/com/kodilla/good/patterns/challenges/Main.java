e1a027527afb2cf59f87d9677ef913c255738f2c
zieluzieluzielu <mac.ziel@gmail.com>
2019-12-16 20:37:11 +0100

    Module 13.1

--- src/main/java/com/kodilla/good/patterns/challenges/Main.java
+++ src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -1,14 +1,16 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 public class Main {
 
     public static  void main (String[] args) {
         MovieStore movieStore = new MovieStore();
 
+        String movieList = movieStore.getMovies().values().stream().flatMap(List::stream).collect(Collectors.joining("!"));
 
-        String moviesList = movieStore.getMovies().values().stream().map(Object::toString).map(Object::toString).collect(Collectors.joining("!")).replace("[", "").replace("]", "").replace(", ", "!");
-        System.out.println(moviesList);
+        System.out.println(movieList);
     }
 
 }
